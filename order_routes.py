9c6c83da7008dce1cbf2ebaf9c27fe97c6bf6fb7
KARTHIKEYAN R <90916934+rKarthik67@users.noreply.github.com>
2024-03-11 11:50:20 +0530

    new changes 11/3/24

--- order_routes.py
+++ order_routes.py
@@ -56,3 +56,76 @@ async def place_an_order(order:OrderModel,Authorize:AuthJWT=Depends()):
     }
 
     return jsonable_encoder(response)
+
+
+@order_router.get('/orders')
+async def list_all_orders(Authorize:AuthJWT=Depends()):
+    """
+        ## List all orders
+        This lists all  orders made. It can be accessed by superusers
+        
+    
+    """
+
+
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+
+    current_user=Authorize.get_jwt_subject()
+
+    user=session.query(User).filter(User.username==current_user).first()
+
+    if user.is_staff:
+        orders=session.query(Order).all()
+
+        return jsonable_encoder(orders)
+
+    raise  HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are not a superuser"
+        )
+
+@order_router.get('/orders/{id}')
+async def get_order_by_id(id:int,Authorize:AuthJWT=Depends()):
+
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+    
+    user=Authorize.get_jwt_subject()
+
+    current_user=session.query(User).filter(User.username==user).first()
+
+    if current_user.is_staff:
+        order=session.query(Order).filter(Order.id==id).first()
+
+        return jsonable_encoder(order)
+    
+    raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not alowed to carry out request"
+        )
+
+
+@order_router.get('/user/orders')
+async def get_user_orders(Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+
+    user=Authorize.get_jwt_subject()
+
+    current_user = session.query(User).filter(User.username==user).first()
+
+    return jsonable_encoder(current_user.orders)
